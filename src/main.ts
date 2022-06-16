418db1dbd19dba29d8ae539b2a8067cc0a42b063
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-16 13:34:33 +0100

    Rotating torus

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-import './style.css'
+import './style.css';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+import * as THREE from 'three';
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg') as HTMLCanvasElement,
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geomery = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({color: 0xFF64347, wireframe: true})
+const torus = new THREE.Mesh(geomery, material);
+scene.add(torus);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
