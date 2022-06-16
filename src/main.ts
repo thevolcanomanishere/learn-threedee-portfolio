b2ddf1dc7a092b5a545631646baf3412552a14ef
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-16 14:21:37 +0100

    Add readme, github pages

--- src/main.ts
+++ src/main.ts
@@ -39,11 +39,12 @@ const addStar = () => {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({color: 0xFFFFFF});
   const star = new THREE.Mesh(geometry, material);
+  // @ts-ignore
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
   star.position.set(x, y, z);
   scene.add(star);
 }
-
+  // @ts-ignore
 Array(400).fill().forEach(addStar);
 
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
